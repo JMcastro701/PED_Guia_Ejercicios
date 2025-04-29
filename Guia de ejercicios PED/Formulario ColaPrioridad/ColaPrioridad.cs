04da57489cb8b1c2f5bc1e076684d48625c9a204
JMcastro701 <rorsos1395@gmail.com>
2025-04-29 12:12:24 -0600

    Add files via upload

---
+++ Guia de ejercicios PED/Formulario ColaPrioridad/ColaPrioridad.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario_ColaPrioridad
+{
+    class ColaPrioridad
+    {
+        private NodoCola frente;
+            private int contadorPacientes = 0;
+
+        public void Encolar(int prioridad)
+        {
+            contadorPacientes++;
+                NodoCola nuevoNodo = new NodoCola(contadorPacientes, prioridad);
+
+            if (frente == null || prioridad < frente.Prioridad)
+            {
+                //Colocamos nuevo nodo en la primera posición
+                nuevoNodo.Siguiente = frente;
+                    frente = nuevoNodo;
+            }
+            else
+            {
+                //Buscamos la posición en la que insertar el nuevo nodo
+                NodoCola actual = frente;
+                    while (actual.Siguiente != null && actual.Siguiente.Prioridad <= prioridad)
+                    {
+                        actual = actual.Siguiente;
+                    }
+
+                    nuevoNodo.Siguiente = actual.Siguiente;
+                        actual.Siguiente = nuevoNodo;
+            }
+        }
+
+        public int Desencolar()
+        {
+            if (frente == null)
+                //Regresamos valor que representará un error
+                return -1;
+                    //Guardamos número del paciente a desencolar
+                        int numeroPaciente = frente.NumeroPaciente;
+
+                        //Desconectamos nodo de la cola
+                            frente = frente.Siguiente;
+
+                            //Retornamos número del paciente que es desencolado
+                                return numeroPaciente;
+        }
+
+        public void MostrarCola(DataGridView dgvOutput)
+        {
+            dgvOutput.Rows.Clear();
+                dgvOutput.Columns.Clear();
+                    dgvOutput.Columns.Add("numeroPaciente", "Número de Paciente");
+                        dgvOutput.Columns.Add("prioridad", "Prioridad");
+
+                    NodoCola actual = frente;
+                        while (actual != null)
+                        {
+                            dgvOutput.Rows.Add(actual.NumeroPaciente, actual.Prioridad);
+                            actual = actual.Siguiente;
+                        }
+        }
+
+    }
+}
---
+++ Guia de ejercicios PED/Formulario ColaPrioridad/FrmColaP.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace Formulario_ColaPrioridad
+{
+    partial class FrmColaP
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPacientes = new System.Windows.Forms.DataGridView();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxEstado = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnAtender = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxAtendidos = new System.Windows.Forms.TextBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPacientes)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvPacientes
+            // 
+            this.dgvPacientes.BackgroundColor = System.Drawing.Color.SkyBlue;
+            this.dgvPacientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPacientes.Location = new System.Drawing.Point(523, 115);
+            this.dgvPacientes.Name = "dgvPacientes";
+            this.dgvPacientes.Size = new System.Drawing.Size(648, 464);
+            this.dgvPacientes.TabIndex = 0;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(162, 102);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(123, 37);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar Paciente";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(454, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Gestión de Pacientes";
+            // 
+            // comboBoxEstado
+            // 
+            this.comboBoxEstado.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxEstado.FormattingEnabled = true;
+            this.comboBoxEstado.Location = new System.Drawing.Point(257, 40);
+            this.comboBoxEstado.Name = "comboBoxEstado";
+            this.comboBoxEstado.Size = new System.Drawing.Size(177, 33);
+            this.comboBoxEstado.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(1, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1170, 46);
+            this.panel1.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBoxEstado);
+            this.groupBox1.Location = new System.Drawing.Point(26, 123);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(461, 159);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(206, 25);
+            this.label3.TabIndex = 10;
+            this.label3.Text = " Prioridad del Paciente:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Location = new System.Drawing.Point(523, 48);
+            this.panel2.Margin = new System.Windows.Forms.Padding(5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(648, 70);
+            this.panel2.TabIndex = 8;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(195, 17);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(273, 32);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "PACIENTES EN ESPERA\r\n";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnAtender);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textBoxAtendidos);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(26, 300);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(461, 186);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnAtender
+            // 
+            this.btnAtender.Location = new System.Drawing.Point(108, 32);
+            this.btnAtender.Name = "btnAtender";
+            this.btnAtender.Size = new System.Drawing.Size(197, 30);
+            this.btnAtender.TabIndex = 11;
+            this.btnAtender.Text = "Atender Paciente";
+            this.btnAtender.UseVisualStyleBackColor = true;
+            this.btnAtender.Click += new System.EventHandler(this.btnAtender_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 76);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(169, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Paciente Atendido:";
+            // 
+            // textBoxAtendidos
+            // 
+            this.textBoxAtendidos.Location = new System.Drawing.Point(210, 73);
+            this.textBoxAtendidos.Multiline = true;
+            this.textBoxAtendidos.Name = "textBoxAtendidos";
+            this.textBoxAtendidos.ReadOnly = true;
+            this.textBoxAtendidos.Size = new System.Drawing.Size(215, 95);
+            this.textBoxAtendidos.TabIndex = 12;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(197, 492);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(96, 32);
+            this.btnLimpiar.TabIndex = 13;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // FrmColaP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(1171, 579);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dgvPacientes);
+            this.Name = "FrmColaP";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPacientes)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPacientes;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxEstado;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnAtender;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxAtendidos;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
